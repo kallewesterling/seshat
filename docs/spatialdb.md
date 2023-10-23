2606942906fb074320bedbac8f6db2d526b5256c
Ed Chalstrey <edwardchalstrey@gmail.com>
2023-10-23 12:00:41 +0100

    use shp2pgsql

--- docs/spatialdb.md
+++ docs/spatialdb.md
@@ -11,3 +11,12 @@
     ```
         CREATE EXTENSION postgis;
     ```
+
+<!-- 3. Creating a script (and subsequent table) from an individual shapefile [with shp2pgsql](https://www.crunchydata.com/blog/loading-data-into-postgis-an-overview#shp2pgsql) results in a single row/geom in that table -->
+
+4. Script that uses `shp2pgsql`
+    ```
+        cd spatialdb
+        chmod +x ./macrostate_shapefiles.sh
+        ./macrostate_shapefiles.sh /path/to/dir seshat_spatial <table_name>
+    ```
---
+++ spatialdb/macrostate_shapefiles.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Check for the correct number of arguments
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 <base_dir> <database_name> <table_name>"
+  exit 1
+fi
+
+# Set script arguments to variables
+base_dir="$1"
+database_name="$2"
+table_name="$3"
+
+# Loop through subdirectories
+for dir in "$base_dir"/*/; do
+  # Extract the directory name
+  subdir_name=$(basename "$dir")
+
+  # Define the path to the .shp file (assuming the Shapefile has the same name as the subdirectory)
+  shp_file="${dir}${subdir_name}.shp"
+
+  # Use shp2pgsql to convert the Shapefile to SQL and append it to the specified table
+  shp2pgsql -a -s 4326 -I -c "$shp_file" "$table_name" >> macrostate_shapefiles.sql
+done
+
+# Once the loop is complete, you can run the generated SQL script
+echo "Run the generated SQL script (macrostate_shapefiles.sql) to populate the database."
