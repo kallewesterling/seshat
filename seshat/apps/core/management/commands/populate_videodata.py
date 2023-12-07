7f83bde2ee81b344bdb2fb96d5d497585d166e46
Ed Chalstrey <edwardchalstrey@gmail.com>
2023-12-07 14:49:23 +0000

    shapely version

--- seshat/apps/core/management/commands/populate_videodata.py
+++ seshat/apps/core/management/commands/populate_videodata.py
@@ -1,14 +1,10 @@
 import os
-from django.core.management.base import BaseCommand
-from django.contrib.gis.geos import GEOSGeometry
-from seshat.apps.core.models import VideoShapefile
 import geopandas as gpd
+from django.core.management.base import BaseCommand
+from seshat.apps.core.models import VideoShapefile
+from shapely.geometry import shape
 
 class Command(BaseCommand):
-    """
-    Iterate through geojson files in the provided directory.
-    Populate the core_videoshapefile table with the geojson fields.
-    """
     help = 'Populates the database with Shapefiles'
 
     def add_arguments(self, parser):
@@ -17,33 +13,24 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         dir = options['dir']
 
-        # Iterate through each file in the base directory
-        for file_name in os.listdir(dir):
-            if file_name.endswith(".geojson"):
-                file_path = os.path.join(dir, file_name)
+        for filename in os.listdir(dir):
+            if filename.endswith('.geojson'):
+                filepath = os.path.join(dir, filename)
 
-                # Read GeoJSON file using geopandas
-                gdf = gpd.read_file(file_path)
+                # Load GeoJSON file using geopandas
+                gdf = gpd.read_file(filepath)
 
-                # Iterate through features and populate VideoShapefile model
-                for _, row in gdf.iterrows():
-                    geom = GEOSGeometry(row.geometry)
-                    name = row.Name
-                    name_underscores = row.PolID
-                    start_year = row.Year
-                    area = row.Area_km2
-                    seshat_id = row.SeshatID
-                    wikipedia_name = row.Wikipedia
-
-                    # Create VideoShapefile instance and save to the database
-                    VideoShapefile.objects.create(
-                        geom=geom,
-                        name=name,
-                        name_underscores=name_underscores,
-                        start_year=start_year,
-                        area=area,
-                        seshat_id=seshat_id,
-                        wikipedia_name=wikipedia_name
+                for index, row in gdf.iterrows():
+                    # Extract fields from GeoDataFrame and create VideoShapefile object
+                    video_shapefile = VideoShapefile(
+                        geom=shape(row.geometry),
+                        name=row['Name'],
+                        name_underscores=row['PolID'],
+                        start_year=row['Year'],
+                        area=row['Area_km2'],
+                        seshat_id=row['SeshatID'],
+                        wikipedia_name=row['Wikipedia'],
                     )
+                    video_shapefile.save()
 
-                self.stdout.write(self.style.SUCCESS(f'Successfully imported {file_name}'))
+                self.stdout.write(self.style.SUCCESS(f'Successfully loaded data from {filename}'))
