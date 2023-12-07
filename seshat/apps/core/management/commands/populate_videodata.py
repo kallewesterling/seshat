6d26cff9d4fe4cd70cb43b46b152fc2476a92cb0
Ed Chalstrey <edwardchalstrey@gmail.com>
2023-12-07 13:25:03 +0000

    suggestion

--- seshat/apps/core/management/commands/populate_videodata.py
+++ seshat/apps/core/management/commands/populate_videodata.py
@@ -1,8 +1,8 @@
 import os
-from django.contrib.gis.gdal import DataSource
-from django.contrib.gis.geos import GEOSGeometry, MultiPolygon
 from django.core.management.base import BaseCommand
+from django.contrib.gis.geos import GEOSGeometry
 from seshat.apps.core.models import VideoShapefile
+import geopandas as gpd
 
 class Command(BaseCommand):
     """
@@ -16,3 +16,34 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         base_dir = options['base_dir']
+
+        # Iterate through each file in the base directory
+        for file_name in os.listdir(base_dir):
+            if file_name.endswith(".geojson"):
+                file_path = os.path.join(base_dir, file_name)
+
+                # Read GeoJSON file using geopandas
+                gdf = gpd.read_file(file_path)
+
+                # Iterate through features and populate VideoShapefile model
+                for _, row in gdf.iterrows():
+                    geom = GEOSGeometry(row.geometry)
+                    name = row.Name
+                    name_underscores = row.PolID
+                    start_year = row.Year
+                    area = row.Area_km2
+                    seshat_id = row.SeshatID
+                    wikipedia_name = row.Wikipedia
+
+                    # Create VideoShapefile instance and save to the database
+                    VideoShapefile.objects.create(
+                        geom=geom,
+                        name=name,
+                        name_underscores=name_underscores,
+                        start_year=start_year,
+                        area=area,
+                        seshat_id=seshat_id,
+                        wikipedia_name=wikipedia_name
+                    )
+
+                self.stdout.write(self.style.SUCCESS(f'Successfully imported {file_name}'))
