02e52e2284e936c1c710fd18e53557c19183b980
Ed Chalstrey <edwardchalstrey@gmail.com>
2023-12-08 10:19:10 +0000

    make end_year allow null

--- seshat/apps/core/models.py
+++ seshat/apps/core/models.py
@@ -637,13 +637,13 @@ class MacrostateShapefile(models.Model):
 
 class VideoShapefile(models.Model):
     geom = models.MultiPolygonField()
-    name=models.CharField(max_length=100, null=True)
-    name_underscores=models.CharField(max_length=100, null=True)
+    name=models.CharField(max_length=100)
+    name_underscores=models.CharField(max_length=100)
     wikipedia_name=models.CharField(max_length=100, null=True)
-    seshat_id=models.CharField(max_length=100, blank=True, null=True)
+    seshat_id=models.CharField(max_length=100)
     area=models.FloatField()
     start_year = models.IntegerField()
-    end_year = models.IntegerField()
+    end_year = models.IntegerField(null=True)
 
     def __str__(self):
         return "Name: %s" % self.name
