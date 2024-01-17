6674ce61d2d8ab7cc391dfb00fa0fab6e6043729
Ed Chalstrey <edwardchalstrey@gmail.com>
2024-01-17 16:04:58 +0000

    use local or prod settings based on presence of .env

--- seshat/asgi.py
+++ seshat/asgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+from pathlib import Path
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seshat.settings.local')
+local_env_path = str(Path.cwd()) + "/seshat/settings/.env"
+if os.path.exists(local_env_path):
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                            'seshat.settings.local')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                            'seshat.settings.production')
 
 application = get_asgi_application()
