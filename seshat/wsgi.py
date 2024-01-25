605deb70b68ef4956b9a38870bdf27faa337b5b1
Ed Chalstrey <edwardchalstrey@gmail.com>
2024-01-25 14:32:35 +0000

    Refactor import in wsgi.py

--- seshat/wsgi.py
+++ seshat/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
+from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
 #from whitenoise.django import DjangoWhiteNoise
