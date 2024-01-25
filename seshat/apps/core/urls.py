26d87ba269c7357fcf3c83d0662915cc8cc0e6db
Ed Chalstrey <edwardchalstrey@gmail.com>
2024-01-25 10:24:52 +0000

    Remove gadm_map_view

--- seshat/apps/core/urls.py
+++ seshat/apps/core/urls.py
@@ -145,4 +145,3 @@ urlpatterns += [
 urlpatterns += [path('core/spatial_map/', views.map_view_initial, name='spatial_map'),]
 urlpatterns += [path('core/spatial_map_all/', views.map_view_all, name='spatial_map_all'),]
 urlpatterns += [path('core/provinces_and_countries', views.provinces_and_countries_view, name='provinces_and_countries'),]
-urlpatterns += [path('core/gadm_map/', views.gadm_map_view, name='gadm_map'),]
