781c178c85c42f3ae3f16229c4e5b24cc56fc228
Ed Chalstrey <edwardchalstrey@gmail.com>
2024-01-25 14:29:49 +0000

    Update DJANGO_SETTINGS_MODULE in wsgi.py

--- seshat/wsgi.py
+++ seshat/wsgi.py
@@ -8,11 +8,16 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 #from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seshat.settings')
+local_env_path = str(Path.cwd()) + "/seshat/settings/.env"
+if os.path.exists(local_env_path):
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seshat.settings.local')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seshat.settings.base')
 
 application = get_wsgi_application()
 #application = DjangoWhiteNoise(application)
