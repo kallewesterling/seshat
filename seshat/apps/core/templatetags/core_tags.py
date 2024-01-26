8c64f7af3baa06c32a65fab857e55148a78744af
Ed Chalstrey <edwardchalstrey@gmail.com>
2024-01-26 13:48:44 +0000

    get correct polity for this page

--- seshat/apps/core/templatetags/core_tags.py
+++ seshat/apps/core/templatetags/core_tags.py
@@ -1,12 +1,17 @@
 from django import template
+from ..models import Polity
 from ..views import get_polity_shape_content
 
 register = template.Library()
 
 @register.inclusion_tag('core/polity_map.html')
 def polity_map(pk):
-    # seshat_id = str(pk)
-    seshat_id = "us_united_states_of_america_contemporary"
+    page_id = str(pk)
+    seshat_id = get_seshat_id_from_page_id(page_id)
     content = get_polity_shape_content(seshat_id=seshat_id)
-
     return {'content': content}
+
+
+def get_seshat_id_from_page_id(page_id):
+    polity = Polity.objects.get(id=page_id)
+    return polity.new_name
