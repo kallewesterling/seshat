f6f88a45527472082cfd2ba5ac44a77fd8046212
Ed Chalstrey <edwardchalstrey@gmail.com>
2024-01-31 15:32:52 +0000

    Refactor network configuration to make network_security_group before subnet

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -20,25 +20,6 @@ public_ip = network.PublicIp('publicIp',
     allocation_method='Dynamic'
 )
 
-# Create a subnet and associate it with the network security group
-subnet = network.Subnet('subnet',
-    resource_group_name=resource_group.name,
-    virtual_network_name=virtual_network.name,
-    address_prefix='10.0.2.0/24',
-    network_security_group_id=network_security_group.id,
-)
-
-# Create a network interface
-network_interface = network.NetworkInterface('networkInterface',
-    resource_group_name=resource_group.name,
-    ip_configurations=[{
-        'name': 'webserver',
-        'subnet_id': subnet.id,  # Use the new subnet
-        'private_ip_address_allocation': 'Dynamic',
-        'public_ip_address_id': public_ip.id,
-    }]
-)
-
 # Create a network security group
 network_security_group = network.NetworkSecurityGroup('networkSecurityGroup',
     resource_group_name=resource_group.name,
@@ -60,6 +41,25 @@ network_security_group = network.NetworkSecurityGroup('networkSecurityGroup',
          'destination_port_range': '8000', 'source_address_prefix': '*',
          'destination_address_prefix': '*'},
     ]
+)
+
+# Create a subnet and associate it with the network security group
+subnet = network.Subnet('subnet',
+    resource_group_name=resource_group.name,
+    virtual_network_name=virtual_network.name,
+    address_prefix='10.0.2.0/24',
+    network_security_group_id=network_security_group.id,
+)
+
+# Create a network interface
+network_interface = network.NetworkInterface('networkInterface',
+    resource_group_name=resource_group.name,
+    ip_configurations=[{
+        'name': 'webserver',
+        'subnet_id': subnet.id,  # Use the new subnet
+        'private_ip_address_allocation': 'Dynamic',
+        'public_ip_address_id': public_ip.id,
+    }]
 )
 
 # Update the network interface to use the network security group
