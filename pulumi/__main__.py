be4c8d55324a6565f013231452232f348b8a6c56
Ed Chalstrey <edwardchalstrey@gmail.com>
2024-02-01 14:42:16 +0000

    just pip install

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -105,7 +105,7 @@ cd /home/webadmin/seshat
 git checkout pulumi
 python3.8 -m venv venv
 source venv/bin/activate
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 export DJANGO_SETTINGS_MODULE=seshat.settings.local
 sudo ufw allow 8000
 gunicorn seshat.wsgi:application --config gunicorn.conf.py &
