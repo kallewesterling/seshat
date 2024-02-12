77ff31832152db28da0b23083c0a417a75a40edf
Ed Chalstrey <edwardchalstrey@gmail.com>
2024-02-12 14:45:58 +0000

    Add docstrings

--- seshat/apps/core/tests/tests.py
+++ seshat/apps/core/tests/tests.py
@@ -1,14 +1,8 @@
-from django.contrib.gis.geos import MultiPolygon, Polygon
-from django.test import TestCase, Client
-from django.urls import reverse
-from ..models import VideoShapefile, GADMShapefile, GADMCountries, GADMProvinces
-
-
-# Simple square polygon to use in geospatial data table tests
-square = MultiPolygon(Polygon(((0, 0), (0, 1), (1, 1), (0, 0))))
-
 class ModelTest(TestCase):
+    """Test case for the models."""
+
     def test_video_shapefile_creation(self):
+        """Test the creation of a VideoShapefile instance."""
         video_shapefile = VideoShapefile.objects.create(
             geom=square,
             name="Test shape",
@@ -25,6 +19,7 @@ class ModelTest(TestCase):
         self.assertEqual(video_shapefile.name, "Test shape")
 
     def test_gadm_shapefile_creation(self):
+        """Test the creation of a GADMShapefile instance."""
         gadm_shapefile = GADMShapefile.objects.create(
             geom=square,
             UID=123456789,
@@ -34,6 +29,7 @@ class ModelTest(TestCase):
         self.assertEqual(gadm_shapefile.NAME_0, "Test shape")
 
     def test_gadm_countries_creation(self):
+        """Test the creation of a GADMCountries instance."""
         gadm_countries = GADMCountries.objects.create(
             geom=square,
             COUNTRY="Test Country"
@@ -42,6 +38,7 @@ class ModelTest(TestCase):
         self.assertEqual(gadm_countries.COUNTRY, "Test Country")
 
     def test_gadm_provinces_creation(self):
+        """Test the creation of a GADMProvinces instance."""
         gadm_provinces = GADMProvinces.objects.create(
             geom=square,
             NAME_1="Test Province"
@@ -49,18 +46,25 @@ class ModelTest(TestCase):
         self.assertIsInstance(gadm_provinces, GADMProvinces)
         self.assertEqual(gadm_provinces.NAME_1, "Test Province")
 
+
 class ViewTest(TestCase):
+    """Test case for the views."""
+
     def setUp(self):
+        """Set up the test client for the views."""
         self.client = Client()
 
     def test_map_view_initial(self):
+        """Test the initial map view."""
         response = self.client.get(reverse('world_map'))
         self.assertEqual(response.status_code, 200)
 
     def test_map_view_all(self):
+        """Test the map view with all data."""
         response = self.client.get(reverse('world_map_all'))
         self.assertEqual(response.status_code, 200)
 
     def test_provinces_and_countries_view(self):
+        """Test the provinces and countries view."""
         response = self.client.get(reverse('provinces_and_countries'))
         self.assertEqual(response.status_code, 200)
