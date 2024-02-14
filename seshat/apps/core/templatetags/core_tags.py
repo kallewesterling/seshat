e1a54fd0d60e9f0aee8a742c8a6d0806bb020bfe
Ed Chalstrey <edwardchalstrey@gmail.com>
2024-02-14 12:15:09 +0000

    use django orm for get_polity_capitals

--- seshat/apps/core/templatetags/core_tags.py
+++ seshat/apps/core/templatetags/core_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from django.db import connection
-from ..models import Polity
+from django.db.models import F
+from ..models import Polity, Capital
+from ...general.models import Polity_capital
 from ..views import get_polity_shape_content
 
 register = template.Library()
@@ -24,28 +26,44 @@ def polity_map(pk):
     return {'content': content}
 
 def get_polity_capitals(pk):
-    query = """
-            SELECT gpc.capital, CAST(cc.latitude AS FLOAT), CAST(cc.longitude AS FLOAT), gpc.year_from, gpc.year_to
-            FROM core_polity as cp, general_polity_capital as gpc, core_capital as cc
-            WHERE cp.id = gpc.polity_id
-            AND cc.name = gpc.capital
-            and cp.id=%s;
-            """
     capitals_info = []
-    with connection.cursor() as cursor:
-        cursor.execute(query, [pk])
-        rows = cursor.fetchall()
-    for row in rows:
-        cap_dict = {}
-        if row[0] and row[1] and row[2]:
-            cap_dict['capital'] = row[0]
-            cap_dict['latitude'] = row[1]
-            cap_dict['longitude'] = row[2]
-            # Only a small number of capitals have a year_from or year_to
-            # TODO: None of the seshat pages with shape data currently have multiple capitals split by time
-            # if row[3]:
-            #     cap_dict['year_from'] = row[3]
-            # if row[4]:
-            #     cap_dict['year_to'] = row[4]
-            capitals_info.append(cap_dict)
+    polity_capitals = Polity_capital.objects.filter(polity_id=pk)
+    
+    for polity_capital in polity_capitals:
+        capitals = Capital.objects.filter(name=polity_capital.capital)
+        for capital in capitals:
+            capital_info = {}
+            if capital.name and capital.latitude and capital.longitude:
+                capital_info['capital'] = capital.name
+                capital_info['latitude'] = float(capital.latitude)
+                capital_info['longitude'] = float(capital.longitude)
+                capitals_info.append(capital_info)
+    
     return capitals_info
+
+# def get_polity_capitals(pk):
+#     query = """
+#             SELECT gpc.capital, CAST(cc.latitude AS FLOAT), CAST(cc.longitude AS FLOAT), gpc.year_from, gpc.year_to
+#             FROM core_polity as cp, general_polity_capital as gpc, core_capital as cc
+#             WHERE cp.id = gpc.polity_id
+#             AND cc.name = gpc.capital
+#             and cp.id=%s;
+#             """
+#     capitals_info = []
+#     with connection.cursor() as cursor:
+#         cursor.execute(query, [pk])
+#         rows = cursor.fetchall()
+#     for row in rows:
+#         cap_dict = {}
+#         if row[0] and row[1] and row[2]:
+#             cap_dict['capital'] = row[0]
+#             cap_dict['latitude'] = row[1]
+#             cap_dict['longitude'] = row[2]
+#             # Only a small number of capitals have a year_from or year_to
+#             # TODO: None of the seshat pages with shape data currently have multiple capitals split by time
+#             # if row[3]:
+#             #     cap_dict['year_from'] = row[3]
+#             # if row[4]:
+#             #     cap_dict['year_to'] = row[4]
+#             capitals_info.append(cap_dict)
+#     return capitals_info
