fa161050bb5d833518128340aaff8cda280b1bd8
Ed Chalstrey <edwardchalstrey@gmail.com>
2024-02-14 10:48:58 +0000

    remove sub func from get_provinces

--- seshat/apps/core/views.py
+++ seshat/apps/core/views.py
@@ -2259,47 +2259,44 @@ def get_provinces(selected_base_map_gadm='province', simplification_tolerance=0.
     # Get all the province or country shapes for the map base layer
     provinces = []
 
-    def fetch_provinces():
-        # Use the appropriate SQL query based on the selected baseMapGADM value
-        if selected_base_map_gadm == 'country':
-            query = """
-                SELECT
-                    ST_Simplify(geom, %s) AS simplified_geometry,
-                    "COUNTRY"                
-                FROM
-                    core_gadmcountries;
-            """
-        elif selected_base_map_gadm == 'province':
-            query = """
-                SELECT
-                    ST_Simplify(geom, %s) AS simplified_geometry,
-                    "NAME_1",
-                    "ENGTYPE_1"                
-                FROM
-                    core_gadmprovinces;
-            """
+    # Use the appropriate SQL query based on the selected baseMapGADM value
+    if selected_base_map_gadm == 'country':
+        query = """
+            SELECT
+                ST_Simplify(geom, %s) AS simplified_geometry,
+                "COUNTRY"                
+            FROM
+                core_gadmcountries;
+        """
+    elif selected_base_map_gadm == 'province':
+        query = """
+            SELECT
+                ST_Simplify(geom, %s) AS simplified_geometry,
+                "NAME_1",
+                "ENGTYPE_1"                
+            FROM
+                core_gadmprovinces;
+        """
 
-        with connection.cursor() as cursor:
-            cursor.execute(query, [simplification_tolerance])
-            rows = cursor.fetchall()
+    with connection.cursor() as cursor:
+        cursor.execute(query, [simplification_tolerance])
+        rows = cursor.fetchall()
 
-        for row in rows:
-            if row[0] != None:
-                if selected_base_map_gadm == 'country':
-                    provinces.append({
-                        'aggregated_geometry': GEOSGeometry(row[0]).geojson,
-                        'country': row[1]
-                    })
-                elif selected_base_map_gadm == 'province':
-                    provinces.append({
-                        'aggregated_geometry': GEOSGeometry(row[0]).geojson,
-                        'province': row[1],
-                        'province_type': row[2]
-                    })
+    for row in rows:
+        if row[0] != None:
+            if selected_base_map_gadm == 'country':
+                provinces.append({
+                    'aggregated_geometry': GEOSGeometry(row[0]).geojson,
+                    'country': row[1]
+                })
+            elif selected_base_map_gadm == 'province':
+                provinces.append({
+                    'aggregated_geometry': GEOSGeometry(row[0]).geojson,
+                    'province': row[1],
+                    'province_type': row[2]
+                })
 
-        return provinces
-
-    return fetch_provinces()
+    return provinces
 
 # Update shapes with polity_id for loading Seshat pages
 def get_polity_info(seshat_ids):
