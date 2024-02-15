9b6539926462d866ac4f988c032e4d43ff598a67
Ed Chalstrey <edwardchalstrey@gmail.com>
2024-02-15 11:36:31 +0000

    add gdal and geos packages

--- seshat/settings/local.py
+++ seshat/settings/local.py
@@ -38,15 +38,15 @@ else:
         }
     }
 
-    # Shapefile spatial stuff
-    GEOGRAPHIC_DB = True
+# Shapefile spatial stuff
+GEOGRAPHIC_DB = True
 
-    if sys.platform.startswith('darwin'): # macOS
-        GDAL_LIBRARY_PATH = '/opt/homebrew/opt/gdal/lib/libgdal.dylib'
-        GEOS_LIBRARY_PATH = '/opt/homebrew/opt/geos/lib/libgeos_c.dylib'
-    else: # linux
-        GDAL_LIBRARY_PATH = '/usr/lib/libgdal.so'
-        GEOS_LIBRARY_PATH = '/usr/lib/aarch64-linux-gnu/libgeos_c.so'
+if sys.platform.startswith('darwin'): # macOS
+    GDAL_LIBRARY_PATH = '/opt/homebrew/opt/gdal/lib/libgdal.dylib'
+    GEOS_LIBRARY_PATH = '/opt/homebrew/opt/geos/lib/libgeos_c.dylib'
+else: # linux
+    GDAL_LIBRARY_PATH = '/usr/lib/libgdal.so'
+    GEOS_LIBRARY_PATH = '/usr/lib/aarch64-linux-gnu/libgeos_c.so'
 
 django_settings_module = os.environ.get('DJANGO_SETTINGS_MODULE')
 
