99cdf764f6da8228d7924bcfe7b54bf6e6632d3f
Ed Chalstrey <edwardchalstrey@gmail.com>
2024-04-04 11:35:50 +0100

    add language and language colour field to VideoShapefile

--- seshat/apps/core/models.py
+++ seshat/apps/core/models.py
@@ -754,6 +754,8 @@ class VideoShapefile(models.Model):
     polity_start_year=models.IntegerField()
     polity_end_year=models.IntegerField()
     colour=models.CharField(max_length=7)
+    language = models.CharField(max_length=500, null=True)
+    language_colour=models.CharField(max_length=7, null=True)
 
     def __str__(self):
         return "Name: %s" % self.name
