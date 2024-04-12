e58a65880565bf1c2eeaa1451c9be56c99ae4352
Ed Chalstrey <edwardchalstrey@gmail.com>
2024-04-12 10:10:33 +0100

    use a polity_shapes dict instead of iterating through geojson twice

--- seshat/apps/core/management/commands/populate_videodata.py
+++ seshat/apps/core/management/commands/populate_videodata.py
@@ -40,6 +40,8 @@ class Command(BaseCommand):
         polity_years = {}
         # Set of all polities, for generating colour mapping
         all_polities = set()
+        # Dict of all the polities found and the shapes they include
+        polity_shapes = {}
         # Iterate over files in the directory
         for filename in os.listdir(dir):
             if filename.endswith('.geojson'):
@@ -69,10 +71,13 @@ class Command(BaseCommand):
                             if polity_id not in polity_years:
                                 polity_years[polity_id] = []
                             polity_years[polity_id].append(properties['Year'])
+                            if polity_id not in polity_shapes:
+                                polity_shapes[polity_id] = []
+                            polity_shapes[polity_id].append(properties)
 
                             all_polities.add(polity_id)
 
-                            self.stdout.write(self.style.SUCCESS(f'Found shape for {properties["Name"]} ({properties["Year"]})'))
+                            self.stdout.write(self.style.SUCCESS(f'Found shape for {polity_id} ({properties["Year"]})'))
 
         # Sort the polities and generate a colour mapping
         unique_polities = sorted(all_polities)
@@ -80,71 +85,54 @@ class Command(BaseCommand):
         pol_col_map = polity_colour_mapping(unique_polities)
         self.stdout.write(self.style.SUCCESS(f'Colour mapping generated'))
 
-        # Iterate over files in the directory
-        for filename in os.listdir(dir):
-            if filename.endswith('.geojson'):
-                file_path = os.path.join(dir, filename)
+        # Iterate through polity_shapes and create VideoShapefile instances
+        for polity_id, properties in polity_shapes.items():
+            self.stdout.write(self.style.SUCCESS(f'Importing shape for {polity_id} ({properties["Year"]})'))
+            
+            # Get a sorted list of the shape years this polity
+            this_polity_years = sorted(polity_years[polity_id])
 
-                # Read and parse the GeoJSON file
-                with open(file_path, 'r') as geojson_file:
-                    geojson_data = json.load(geojson_file)
+            # Get the polity start and end years
+            polity_start_year = this_polity_years[0]
+            polity_end_year = this_polity_years[-1]
 
-                # Extract data and create VideoShapefile instances
-                for feature in geojson_data['features']:
-                    properties = feature['properties']
-                    if properties['Type'] == 'POLITY':
+            # Get the end year for a shape    
+            # Most of the time, the shape end year is the year of the next shape
+            # Some polities have a gap in their active years
+            # For a shape year at the start of a gap, set the end year to be the shape year, so it doesn't cover the inactive period
+            start_end_years = name_years[properties['Name']]
+            end_years = [x[1] for x in start_end_years]
+            if properties['Year'] in end_years:
+                end_year = properties['Year']
+            else:
+                this_year_index = this_polity_years.index(properties['Year'])
+                try:
+                    end_year = this_polity_years[this_year_index + 1] - 1
+                except IndexError:
+                    end_year = polity_end_year
+            
+            # Save geom and convert Polygon to MultiPolygon if necessary
+            geom = GEOSGeometry(json.dumps(feature['geometry']))
+            if geom.geom_type == 'Polygon':
+                geom = MultiPolygon(geom)
 
-                        try:
-                            polity_id = properties['PolID']
-                        except KeyError:
-                            polity_id = properties['Name'].replace(' ', '_')
+            VideoShapefile.objects.create(
+                geom=geom,
+                name=properties['Name'],
+                polity=polity_id,
+                wikipedia_name=properties['Wikipedia'],
+                seshat_id=properties['SeshatID'],
+                area=properties['Area_km2'],
+                start_year=properties['Year'],
+                end_year=end_year,
+                polity_start_year=polity_start_year,
+                polity_end_year=polity_end_year,
+                colour=pol_col_map[polity_id]
+            )
 
-                        self.stdout.write(self.style.SUCCESS(f'Importing shape for {properties["Name"]} ({properties["Year"]})'))
-                        
-                        # Get a sorted list of the shape years this polity
-                        this_polity_years = sorted(polity_years[polity_id])
+            self.stdout.write(self.style.SUCCESS(f'Successfully imported shape for {properties["Name"]} ({properties["Year"]})'))
 
-                        # Get the polity start and end years
-                        polity_start_year = this_polity_years[0]
-                        polity_end_year = this_polity_years[-1]
-
-                        # Get the end year for a shape    
-                        # Most of the time, the shape end year is the year of the next shape
-                        # Some polities have a gap in their active years
-                        # For a shape year at the start of a gap, set the end year to be the shape year, so it doesn't cover the inactive period
-                        start_end_years = name_years[properties['Name']]
-                        end_years = [x[1] for x in start_end_years]
-                        if properties['Year'] in end_years:
-                            end_year = properties['Year']
-                        else:
-                            this_year_index = this_polity_years.index(properties['Year'])
-                            try:
-                                end_year = this_polity_years[this_year_index + 1] - 1
-                            except IndexError:
-                                end_year = polity_end_year
-                        
-                        # Save geom and convert Polygon to MultiPolygon if necessary
-                        geom = GEOSGeometry(json.dumps(feature['geometry']))
-                        if geom.geom_type == 'Polygon':
-                            geom = MultiPolygon(geom)
-
-                        VideoShapefile.objects.create(
-                            geom=geom,
-                            name=properties['Name'],
-                            polity=polity_id,
-                            wikipedia_name=properties['Wikipedia'],
-                            seshat_id=properties['SeshatID'],
-                            area=properties['Area_km2'],
-                            start_year=properties['Year'],
-                            end_year=end_year,
-                            polity_start_year=polity_start_year,
-                            polity_end_year=polity_end_year,
-                            colour=pol_col_map[polity_id]
-                        )
-
-                        self.stdout.write(self.style.SUCCESS(f'Successfully imported shape for {properties["Name"]} ({properties["Year"]})'))
-
-                self.stdout.write(self.style.SUCCESS(f'Successfully imported all data from {filename}'))
+        self.stdout.write(self.style.SUCCESS(f'Successfully imported all data from {filename}'))
 
         self.stdout.write(self.style.SUCCESS('Adding simplified geometries for faster loading...'))
         # Adjust the tolerance param of ST_Simplify as needed
