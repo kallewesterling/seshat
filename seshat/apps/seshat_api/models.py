251ffb049e0660b7047d0a2a88b87c22363a6f7d
Kalle Westerling <kalle.westerling@gmail.com>
2024-06-13 18:30:20 +0100

    Adding in comments about 2 models in `seshat_api` app

--- seshat/apps/seshat_api/models.py
+++ seshat/apps/seshat_api/models.py
@@ -2,11 +2,33 @@ from django.db import models
 
 
 class Album(models.Model):
+    """
+    :private:
+    :noindex:
+
+    Note:
+        TODO
+
+        This model is used to store information about an album.
+
+        It does not seem to belong to the codebase. Remove it?
+    """
     album_name = models.CharField(max_length=100)
     artist = models.CharField(max_length=100)
 
 
 class Track(models.Model):
+    """
+    :private:
+    :noindex:
+
+    Note:
+        TODO
+
+        This model is used to store information about tracks in an album.
+
+        It does not seem to belong to the codebase. Remove it?
+    """
     album = models.ForeignKey(
         Album, related_name='tracks', on_delete=models.CASCADE)
     order = models.IntegerField()
