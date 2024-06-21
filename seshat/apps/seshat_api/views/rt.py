65d42b546cbd8587173cc4926d8857fe9e701e81
Kalle Westerling <kalle.westerling@gmail.com>
2024-06-21 15:19:06 +0100

    Fixing docstrings in `rt` viewsets

--- seshat/apps/seshat_api/views/rt.py
+++ seshat/apps/seshat_api/views/rt.py
@@ -65,7 +65,7 @@ class TheoSyncDifRelViewSet(
     MixinSeshatAPISerializer, MixinSeshatAPIAuth, viewsets.ModelViewSet
 ):
     """
-    A viewset for viewing and editing Theo Sync Dif Rels.
+    A viewset for viewing and editing Theological Syncretism of Different Religions.
     """
     model = Theo_sync_dif_rel
     pagination_class = SeshatAPIPagination
@@ -75,7 +75,7 @@ class SyncRelPraIndBeliViewSet(
     MixinSeshatAPISerializer, MixinSeshatAPIAuth, viewsets.ModelViewSet
 ):
     """
-    A viewset for viewing and editing Sync Rel Pra Ind Belis.
+    A viewset for viewing and editing Syncretism of Religious Practices at the Level of Individual Believers.
     """
     model = Sync_rel_pra_ind_beli
     pagination_class = SeshatAPIPagination
@@ -95,7 +95,7 @@ class GovVioFreqRelGrpViewSet(
     MixinSeshatAPISerializer, MixinSeshatAPIAuth, viewsets.ModelViewSet
 ):
     """
-    A viewset for viewing and editing Gov Vio Freq Rel Grps.
+    A viewset for viewing and editing Frequency of Governmental Violence Against Religious Groups.
     """
     model = Gov_vio_freq_rel_grp
     pagination_class = SeshatAPIPagination
@@ -105,7 +105,7 @@ class GovResPubWorViewSet(
     MixinSeshatAPISerializer, MixinSeshatAPIAuth, viewsets.ModelViewSet
 ):
     """
-    A viewset for viewing and editing Gov Res Pub Wors.
+    A viewset for viewing and editing Government Restrictions on Public Worships.
     """
     model = Gov_res_pub_wor
     pagination_class = SeshatAPIPagination
@@ -115,7 +115,7 @@ class GovResPubProsViewSet(
     MixinSeshatAPISerializer, MixinSeshatAPIAuth, viewsets.ModelViewSet
 ):
     """
-    A viewset for viewing and editing Gov Res Pub Proses.
+    A viewset for viewing and editing Government Restrictions on Public Proselytizings.
     """
     model = Gov_res_pub_pros
     pagination_class = SeshatAPIPagination
@@ -125,7 +125,7 @@ class GovResConvViewSet(
     MixinSeshatAPISerializer, MixinSeshatAPIAuth, viewsets.ModelViewSet
 ):
     """
-    A viewset for viewing and editing Gov Res Convs.
+    A viewset for viewing and editing Government Restrictions on Conversions.
     """
     model = Gov_res_conv
     pagination_class = SeshatAPIPagination
@@ -135,7 +135,7 @@ class GovPressConvViewSet(
     MixinSeshatAPISerializer, MixinSeshatAPIAuth, viewsets.ModelViewSet
 ):
     """
-    A viewset for viewing and editing Gov Press Convs.
+    A viewset for viewing and editing Government Pressures to Converts.
     """
     model = Gov_press_conv
     pagination_class = SeshatAPIPagination
@@ -145,7 +145,7 @@ class GovResPropOwnForRelGrpViewSet(
     MixinSeshatAPISerializer, MixinSeshatAPIAuth, viewsets.ModelViewSet
 ):
     """
-    A viewset for viewing and editing Gov Res Prop Own For Rel Grps.
+    A viewset for viewing and editing Government Restrictions on Property Ownership for Adherents of and Religious Groups.
     """
     model = Gov_res_prop_own_for_rel_grp
     pagination_class = SeshatAPIPagination
@@ -155,7 +155,7 @@ class TaxRelAdhActInsViewSet(
     MixinSeshatAPISerializer, MixinSeshatAPIAuth, viewsets.ModelViewSet
 ):
     """
-    A viewset for viewing and editing Tax Rel Adh Act Inses.
+    A viewset for viewing and editing Taxes Based on Religious Adherence or on Religious Activities and Institutions.
     """
     model = Tax_rel_adh_act_ins
     pagination_class = SeshatAPIPagination
@@ -165,7 +165,7 @@ class GovOblRelGrpOfcRecoViewSet(
     MixinSeshatAPISerializer, MixinSeshatAPIAuth, viewsets.ModelViewSet
 ):
     """
-    A viewset for viewing and editing Gov Obl Rel Grp Ofc Recos.
+    A viewset for viewing and editing Governmental Obligations for Religious Groups to Apply for Official Recognitions.
     """
     model = Gov_obl_rel_grp_ofc_reco
     pagination_class = SeshatAPIPagination
@@ -175,7 +175,7 @@ class GovResConsRelBuilViewSet(
     MixinSeshatAPISerializer, MixinSeshatAPIAuth, viewsets.ModelViewSet
 ):
     """
-    A viewset for viewing and editing Gov Res Cons Rel Buils.
+    A viewset for viewing and editing Government Restrictions on Construction of Religious Buildings.
     """
     model = Gov_res_cons_rel_buil
     pagination_class = SeshatAPIPagination
@@ -185,7 +185,7 @@ class GovResRelEduViewSet(
     MixinSeshatAPISerializer, MixinSeshatAPIAuth, viewsets.ModelViewSet
 ):
     """
-    A viewset for viewing and editing Gov Res Rel Edus.
+    A viewset for viewing and editing Government Restrictions on Religious Education.
     """
     model = Gov_res_rel_edu
     pagination_class = SeshatAPIPagination
@@ -195,7 +195,7 @@ class GovResCirRelLitViewSet(
     MixinSeshatAPISerializer, MixinSeshatAPIAuth, viewsets.ModelViewSet
 ):
     """
-    A viewset for viewing and editing Gov Res Cir Rel Lits.
+    A viewset for viewing and editing Government Restrictions on Circulation of Religious Literature.
     """
     model = Gov_res_cir_rel_lit
     pagination_class = SeshatAPIPagination
@@ -205,7 +205,7 @@ class GovDisRelGrpOccFunViewSet(
     MixinSeshatAPISerializer, MixinSeshatAPIAuth, viewsets.ModelViewSet
 ):
     """
-    A viewset for viewing and editing Gov Dis Rel Grp Occ Funs.
+    A viewset for viewing and editing Government Discrimination Against Religious Groups Taking Up Certain Occupations or Functions.
     """
     model = Gov_dis_rel_grp_occ_fun
     pagination_class = SeshatAPIPagination
@@ -215,7 +215,7 @@ class SocVioFreqRelGrpViewSet(
     MixinSeshatAPISerializer, MixinSeshatAPIAuth, viewsets.ModelViewSet
 ):
     """
-    A viewset for viewing and editing Soc Vio Freq Rel Grps.
+    A viewset for viewing and editing Social Violence Against Religious Groups.
     """
     model = Soc_vio_freq_rel_grp
     pagination_class = SeshatAPIPagination
@@ -225,7 +225,7 @@ class SocDisRelGrpOccFunViewSet(
     MixinSeshatAPISerializer, MixinSeshatAPIAuth, viewsets.ModelViewSet
 ):
     """
-    A viewset for viewing and editing Soc Dis Rel Grp Occ Funs.
+    A viewset for viewing and editing Social Discrimination Against Religious Groups Taking Up Certain Occupations or Functions.
     """
     model = Soc_dis_rel_grp_occ_fun
     pagination_class = SeshatAPIPagination
@@ -235,7 +235,7 @@ class GovPressConvForAgaViewSet(
     MixinSeshatAPISerializer, MixinSeshatAPIAuth, viewsets.ModelViewSet
 ):
     """
-    A viewset for viewing and editing Gov Press Conv For Agas.
+    A viewset for viewing and editing Societal Pressure to Convert or Against Conversions.
     """
     model = Gov_press_conv_for_aga
     pagination_class = SeshatAPIPagination
